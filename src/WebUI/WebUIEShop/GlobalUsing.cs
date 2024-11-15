3d2362cfca03bd590342b98f0283e64924bb215e
alexmihaita <alexmihaita09@gmail.com>
2024-11-15 11:24:02 +0200

    done

---
+++ src/WebUI/WebUIEShop/GlobalUsing.cs
@@ -0,0 +1,5 @@
+global using Refit;
+global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.AspNetCore.Mvc.RazorPages;
+global using WebUIEShop.Models.Basket;
+global using WebUIEShop.Services;
